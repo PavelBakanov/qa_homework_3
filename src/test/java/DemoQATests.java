f535c2d51cdc62bb8a8165f50d480ce47952f2b0
PavelBakanov <maxconghunter@mail.ru>
2024-09-08 17:19:16 +0300

    homework done =)

--- src/test/java/DemoQATests.java
+++ src/test/java/DemoQATests.java
@@ -31,7 +31,7 @@ public class DemoQATests {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.holdBrowserOpen = true;
+        //Configuration.holdBrowserOpen = true;
     }
     @Test
     void demoQATest()
